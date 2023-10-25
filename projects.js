48df1e988885f8878834444836d38861abb5c3a6
carlosbarreraacuna <carlos.barreraacuna@gmail.com>
2023-10-24 21:36:16 -0500

    Updates

---
+++ projects.js
@@ -0,0 +1,8 @@
+const projects = [
+    {id: 1, name: 'Sistema de atencion psicologica (SAP)', description: 'Dopefolio is a successful Open-Source project that I created which have been featured on some of the biggest tech sites like CSS-Tricks, Hostinger, etc & used by thousands of developers globally', image: '/src/assets/dopefolio.jpeg'},
+    {id: 2, name: 'Pagina web', description: 'Pagina web .', image: '/src/assets/website.png', url: 'https://carlosbarreraacuna.github.io/website1/'},
+    {id: 3, name: 'Almacen y taller MOTOSPA', description: "I re-created the frontend of Boreal Coffee's official web app because I got attracted to their beautiful UI. It was a great experience for me to build the entire frontend.", image: '/src/assets/website.png'},
+    // {id: 4, name: '', description:'', image:'/src/assets/crown-template.jpeg'}
+]
+
+export default projects;
---
+++ skills.js
@@ -0,0 +1,17 @@
+const skills = [
+    {id: 1, image: '/src/assets/icons-skills/html-5.png'},
+    {id: 2, image: '/src/assets/icons-skills/css-3.png'},
+    {id: 3, image: '/src/assets/icons-skills/bootstrap.png'},
+    {id: 4, image: '/src/assets/icons-skills/tailwind.png'},
+    {id: 5, image: '/src/assets/icons-skills/laravel.png'},
+    {id: 6, image: '/src/assets/icons-skills/nodejs.png'},
+    {id: 7, image: '/src/assets/icons-skills/php.png'},
+    {id: 8, image: '/src/assets/icons-skills/git.png'},
+    {id: 9, image: '/src/assets/icons-skills/postgresql.png'},
+    {id: 10, image: '/src/assets/icons-skills/javascript.png'},
+    {id: 11, image: '/src/assets/icons-skills/react.png'},
+    
+
+]
+
+export default skills;
